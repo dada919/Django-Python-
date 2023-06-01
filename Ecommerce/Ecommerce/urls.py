f79de2e2878f86dc6f5f3efdb8ca054b2d5c1877
H3Airness <damienraunier@gmail.com>
2023-06-01 13:49:31 +0200

    ajout page

--- Ecommerce/Ecommerce/urls.py
+++ Ecommerce/Ecommerce/urls.py
@@ -21,5 +21,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('connexion', TemplateView.as_view(template_name='connexion.html'), name='connexion'),
+    path('inscription', TemplateView.as_view(template_name='inscription.html'), name='inscription'),
+    path('panier', TemplateView.as_view(template_name='panier.html'), name='panier'),
 ]
 
