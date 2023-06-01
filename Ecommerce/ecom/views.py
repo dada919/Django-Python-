1730165a6eb14d6d2aa787808fe1c46c994ab0ed
H3Airness <damienraunier@gmail.com>
2023-06-01 14:45:13 +0200

    views.py inscription

--- Ecommerce/ecom/views.py
+++ Ecommerce/ecom/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Membre
+
+def inscription(request):
+    if request.method == 'POST':
+        nom = request.POST.get('nom')
+        mot_de_passe = request.POST.get('mot_de_passe')
+        
+        membre = Membre(nom=nom, mot_de_passe=mot_de_passe)
+        membre.save()
+        
+        return render(request, 'connexion.html')
+    
+    return render(request, 'inscription.html')
+
